0d61a0c10cdff55326bca78cea4fc463cb67dac9
M Xi <bongollmaria@gmail.com>
2025-04-23 13:18:36 +0200

    first back-end commit

---
+++ src/main/java/com/commitme/commit_me/CommitMeApplication.java
@@ -0,0 +1,13 @@
+package com.commitme.commit_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commitme/commit_me/CommitMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commitme.commit_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
