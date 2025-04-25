b8ea6ec30567e554627a1ba093bdb13156281878
M Xi <bongollmaria@gmail.com>
2025-04-25 15:53:46 +0200

    add structure for createSignUp method

--- src/main/java/com/commitme/commit_me/service/SignUpService.java
+++ src/main/java/com/commitme/commit_me/service/SignUpService.java
@@ -1,15 +1,35 @@
 package com.commitme.commit_me.service;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.commitme.commit_me.model.Event;
+import com.commitme.commit_me.model.SignUp;
+import com.commitme.commit_me.model.User;
+import com.commitme.commit_me.repository.EventRepository;
 import com.commitme.commit_me.repository.SignUpRepository;
+import com.commitme.commit_me.repository.UserRepository;
 
 @Service
 public class SignUpService {
 
     private final SignUpRepository signUpRepository;
+    private final UserRepository userRepository;
+    private EventRepository eventRepository;
 
-    public SignUpService (SignUpRepository signUpRepository) {
+    public SignUpService (SignUpRepository signUpRepository, UserRepository userRepository, EventRepository eventRepository) {
         this.signUpRepository = signUpRepository;
+        this.userRepository = userRepository;
+        this.eventRepository = eventRepository;
     }
+
+    public ResponseEntity<Object> createSignUp(Integer userId, SignUp signUp, Integer eventId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        Optional<Event> eventOptional = eventRepository.findById(eventId);
+        if (!userOptional.isPresent() && !eventOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+    }
+
+    // signUp.setUser()
 }
