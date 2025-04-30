d02dff61e3ea25eebdc7063701c7ea55f555bb90
M Xi <bongollmaria@gmail.com>
2025-04-30 15:48:56 +0200

    implement get method in user CRUD

--- src/main/java/com/commitme/commit_me/service/UserService.java
+++ src/main/java/com/commitme/commit_me/service/UserService.java
@@ -1,9 +1,10 @@
 package com.commitme.commit_me.service;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import com.commitme.commit_me.exceptions.EmailAlreadyExistsException;
 import com.commitme.commit_me.model.User;
 import com.commitme.commit_me.repository.UserRepository;
@@ -26,5 +27,20 @@ public class UserService {
         }
         return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
     }
+
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public ResponseEntity<Object> getUserById(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        User user = userOptional.get();
+        return ResponseEntity.ok(user);
+    }
+
 
 }
