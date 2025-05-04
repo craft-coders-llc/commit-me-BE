f4059b042260ea960402befea3f5e9bd86798b2d
Priscila Oliveira <pguillen.oliv@gmail.com>
2025-05-05 00:54:13 +0200

    add global exception handler and categoryAlreadyExistsException

--- src/main/java/com/commitme/commit_me/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/commitme/commit_me/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,40 @@
 package com.commitme.commit_me.exceptions;
 
-public class GlobalExceptionHandler {
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//@RestControllerAdvice
+//public class GlobalExceptionHandler {
     
+    //@ExceptionHandler({MethodArgumentNotValidException.class})
+   // public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception){
+   //     Map<String, String> errors = new HashMap<>();
+    //    exception.getBindingResult().getFieldError().forEach(error -> 
+    //    errors.put(error.getField(), error.getDefaultMessage()));
+   //     return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+   // }
+
+    @RestControllerAdvice
+    public class GlobalExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> 
+            errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CategoryAlreadyExistsException.class)
+    public ResponseEntity<String> handleCategoryAlreadyExistsException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
+
