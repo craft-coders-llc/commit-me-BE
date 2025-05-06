25baea6452eb3228615062d3f890feddbd1bb40f
Priscila Oliveira <pguillen.oliv@gmail.com>
2025-05-06 15:29:22 +0200

    fixed GET all crud of Events

--- src/main/java/com/commitme/commit_me/controller/EventController.java
+++ src/main/java/com/commitme/commit_me/controller/EventController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.commitme.commit_me.model.Event;
 import com.commitme.commit_me.service.EventService;
 import jakarta.validation.Valid;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -29,6 +35,12 @@ public class EventController {
         Integer categoryId = event.getCategory().getId();
         return eventService.createEvent(event, userId, categoryId);
     }
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+    
     
     @PatchMapping("/{id}")
     public ResponseEntity<Object> updateEvent(@PathVariable Integer id, @RequestBody Event updatedEvent){
