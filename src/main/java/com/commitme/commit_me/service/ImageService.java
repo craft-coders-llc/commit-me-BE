0cdd535ca5257b5c3e4317fbb4e1c9e7a9321324
Femcom-Mari <bongollmaria@gmail.com>
2025-05-07 09:32:54 +0200

    Fix store uploaded images in ImagesUpload folder

--- src/main/java/com/commitme/commit_me/service/ImageService.java
+++ src/main/java/com/commitme/commit_me/service/ImageService.java
@@ -4,15 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.commitme.commit_me.model.Image;
 import com.commitme.commit_me.repository.ImageRepository;
-
 import lombok.RequiredArgsConstructor;
-
 
 @RequiredArgsConstructor
 @Service
@@ -20,26 +16,34 @@ public class ImageService {
 
     private final ImageRepository imageRepository;
     private final String FOLDER_PATH = System.getProperty("user.dir") + File.separator + "ImagesUpload";
- 
-        public String uploadImage(MultipartFile file) throws IOException {
-            String filePath = FOLDER_PATH + file.getOriginalFilename();
-            Image image = imageRepository.save(Image.builder()
-            .name(file.getOriginalFilename())
-            .type(file.getContentType())
-            .filePath(filePath).build()
-            );
-            file.transferTo(new File(filePath));
-            if (image != null) {
-                return "file uploaded successfully";
-            }
-            return null;
-    }
-        
-        public byte [] dowloadImageFromFileSystem(String fileName) throws IOException {
-            Optional<Image> image = imageRepository.findByName(fileName);
-            String filePath = image.get().getFilePath();
-            byte[] images =Files.readAllBytes(new File(filePath).toPath());
-            return images;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        File directory = new File(FOLDER_PATH);
+        if (!directory.exists()) {
+            directory.mkdirs(); 
         }
-    
+        String filePath = FOLDER_PATH + File.separator + file.getOriginalFilename();
+        Image image = imageRepository.save(Image.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .filePath(filePath)
+                .build());
+        file.transferTo(new File(filePath));
+        if (image != null) {
+            return "Archivo subido correctamente!";
+        }
+        return "Error al cargar el Archivo";
+    }
+
+    public byte[] dowloadImageFromFileSystem(String fileName) throws IOException {
+        Optional<Image> image = imageRepository.findByName(fileName);
+        if (image.isPresent()) {
+            String filePath = image.get().getFilePath();
+            return Files.readAllBytes(new File(filePath).toPath());
+        } else {
+            throw new IOException("Imagen no encontrada: " + fileName);
+        }
+
+    }
+
 }
